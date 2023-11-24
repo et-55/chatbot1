62974b1331a89e23d603a6d4fc94624d0940fc6e
et-55 <151929125+et-55@users.noreply.github.com>
2023-11-24 22:35:31 +0700

    Update constants.py

--- chatbot-deployment/constants.py
+++ chatbot-deployment/constants.py
@@ -1,3 +1,3 @@
 # Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
 # and rename this file to constants.py.
-APIKEY = "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"
+APIKEY = "sk-"
